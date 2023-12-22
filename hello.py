b88e2178767a61714f67b01657c9c5596e0285bc
Gabriel Barbosa <gaaabriel025@gmail.com>
2023-12-22 19:53:26 -0300

    first version of hello world multi languages script

--- hello.py
+++ hello.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python
+"""
+Hello World multi languages
 
-# Este programa imprime 'Hello, World!'
-print('Hello, World!')
+depending on the language configured in the environment,
+the program displays the corresponding message.
+
+How to use:
+
+You need to have the LANG variable properly configured:
+
+    export LANG=pt_BR
+
+Execution:
+
+    python3 hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Gabriel Barbosa"
+
+import os
+
+current_language = str(os.getenv("LANG", "en_US"))[:5]
+message = "Hello, World!"
+
+if current_language == "pt_BR":
+    message = "Olá, Mundo!"
+elif current_language == "it_IT":
+    message = "Ciao, Mondo!"
+
+print(message)
