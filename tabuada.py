0a6b5a1deab796369799e953607aa81e54bb7469
Gabriel Barbosa <gaaabriel025@gmail.com>
2023-12-26 17:25:25 -0300

    added formatting to output

--- tabuada.py
+++ tabuada.py
@@ -4,8 +4,8 @@ __version__ = '0.1.0'
 __author__ = 'Gabriel'
 
 for first_factor in range(1, 11):
-    print(f'Table of {first_factor}:\n')
+    print("{:#^25}\n".format(f'Table of: {first_factor}'))
     for second_factor in range(1, 11):
         product = first_factor * second_factor
-        print(f'{first_factor} X {second_factor} = {product}')
-    print('-' * 15)
+        print(f'{first_factor:>8} X {second_factor:2} = {product}')
+    print('-' * 25)
