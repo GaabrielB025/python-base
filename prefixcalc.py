f8ac2ef657fca72a58e48ebd60f68519b4f49570
Gabriel Barbosa <gaaabriel025@gmail.com>
2023-12-30 17:23:07 -0300

    exercise prefix calculator V1

---
+++ prefixcalc.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""Calculadora prefix
+
+Funcionamento:
+
+    [operação] [n1] [n2]
+
+Operações:
+    sum -> +
+    sub -> -
+    mul -> *
+    div -> /
+
+Uso:
+    $ prefixcalc.py sum 5 2
+    7
+
+    $ prefixcalc.py mul 10 5
+    50
+
+    $ prefixcalc.py
+    operação: sum
+    n1: 20
+    n2: 10
+    30
+"""
+__version__ = '0.1.0'
+__author__ = 'Gabriel'
+
+import sys
+
+operations = {
+    'sum': lambda a, b: a + b,
+    'sub': lambda a, b: a - b,
+    'mul': lambda a, b: a * b,
+    'div': lambda a, b: a / b,
+}
+
+valid_operations = ('sum', 'sub', 'mul', 'div')
+
+arguments = sys.argv[1:]
+
+# TODO: Exceptions
+if not arguments:
+    operation = input('operation: ').strip()
+    n1 = input('n1: ').strip()
+    n2 = input('n2: ').strip()
+    arguments = [operation, n1, n2]
+elif len(arguments) != 3:
+    print('Number of invalid arguments!')
+    print('You can try: `sum 5 10`')
+    sys.exit(1)
+
+operation, *numbers = arguments
+
+if operation not in operations:
+    print(f'`{operation}` is not a valid operation')
+    print(f'You can choose one of these: {valid_operations}')
+    sys.exit(1)
+
+validated_numbers = []
+for num in numbers:
+    # TODO: Repetição while + exceptions
+    if not num.replace('.', '').isdigit():
+        print(f'Invalid number `{num}`')
+        sys.exit(1)
+
+    if '.' in num:
+        num = float(num)
+    else:
+        num = int(num)
+
+    validated_numbers.append(num)
+
+n1, n2 = validated_numbers
+
+result = operations[operation](n1, n2)
+print(f'The result is: {result:.1f}')
